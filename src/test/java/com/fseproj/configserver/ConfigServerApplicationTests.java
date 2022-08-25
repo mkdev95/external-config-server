393c6afff159ed26eaf8a2014a1ac3a0c5259f5e
unknown <cogjava1206@iiht.tech>
2022-08-25 11:29:16 +0530

    First push

---
+++ src/test/java/com/fseproj/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fseproj.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
